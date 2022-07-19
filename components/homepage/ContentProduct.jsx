7bb5f34b59e01d9a24bad7275c0ddf6f29d130a0
PaulaDuduta <100792374+PaulaDuduta@users.noreply.github.com>
2022-07-19 19:01:01 +0300

    V1

---
+++ components/homepage/ContentProduct.jsx
@@ -0,0 +1,50 @@
+export default function ContentProduct() {
+  return (
+    <section className="content-product container">
+      <aside className="product-images">
+        <div className="screen-image">
+          <img src="images\3.png" alt="watch, wrist, metal, black, style" />
+        </div>
+
+        <div className="carousel-images">
+          <img src="images\6.png" alt="watch, black, metal" />
+
+          <img src="images\7.png" alt="watch, black" />
+
+          <img
+            src="images\jj-ying-dfjfNu3Q0Yc-unsplash.png"
+            alt="watch, black, metal, product-case"
+          />
+
+          <img src="images\6.png" alt="watch, black, metal, leather" />
+        </div>
+      </aside>
+      <div className="product-details">
+        <header>
+          <h3>Monochrome Max Bill - $725</h3>
+        </header>
+
+        <div className="product-description">
+          <div>
+            <h4>description</h4>
+            <p>
+              Mr Max Bill was a true creative genius, carving out a career as a
+              painter, architect, sculptor and designer. As well as leaving
+              behind an expansive body of work, he was also an avid collector of
+              watches, and this Junghans chronoscope timepiece is dedicated to
+              his memory. Immaculately housed in a domed Plexiglass case and
+              finished with a handsome leather strap, it&apos;s a highly
+              technical piece in a stylish package.
+            </p>
+          </div>
+
+          <h4>size & fit</h4>
+
+          <h4>details & care</h4>
+
+          <h4>delivery & returns</h4>
+        </div>
+      </div>
+    </section>
+  );
+}
